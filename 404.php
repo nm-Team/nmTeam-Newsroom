2ecf2ceff1315a2af50332f141e94b4079dba0c6
agoudbg <agoudbg@gmail.com>
2024-09-16 23:48:11 +0800

    feat: redirects new permalink

--- 404.php
+++ 404.php
@@ -1,8 +1,17 @@
 <?php if (!defined('__TYPECHO_ROOT_DIR__')) exit; ?>
 <?php $this->need('header.php'); ?>
 
-<div class="col-mb-12 col-tb-8 col-tb-offset-2">
+<?php
+// The permalink has been changed
+// from /news/{directory}/{cid}-{slug}-{year}-{month}-{day}.html
+// to /news/{cid}-{slug}-{year}-{month}-{day}.html
+if (preg_match('/\/news\/(.+?)\/(.+?)$/', $_SERVER['REQUEST_URI'], $matches)) {
+    $this->response->redirect('/news/' . $matches[2], 301);
+    $this->response->end();
+}
+?>
 
+<div class="col-mb-12 col-tb-8 col-tb-offset-2">
     <div class="error-page">
         <h2 class="post-title">404 - <?php _e('页面没找到'); ?></h2>
         <p><?php _e('你想查看的页面已被转移或删除了, 要不要搜索看看: '); ?></p>
